701d4452b61baf91fc7373aa3c1ea8e84b5f7a79
klimenkoAnna <31847807+klimenkoAnna@users.noreply.github.com>
2017-09-29 00:12:52 +0300

    text processing functions
    The file contains text processing functions

---
+++ word.py
@@ -0,0 +1,32 @@
+from docx import Document
+
+class wordSave:
+	def __init__(self):
+		self.document = Document()
+
+	def reform(self, mass, line)
+		for element in mass:
+			x=line.add_run(element["line"])
+			x.bold = element["bold"]
+			x.italic = element["italic"]
+			x.undetline = element["underline"]
+		
+
+
+	def addHeading(self, mass, lvl=1):
+		line=self.document.add_heading('',level = lvl)
+		self.reform(mass, line)
+
+
+			
+	def addParagraph(self, mass, stylee):
+		line = self.document.add_paragraph(element, style = stylee )
+		self.reform(mass, line)
+
+	#ЧТО ПЕРЕДАЕШЬ??
+	def addPicture(self, mass):
+		line = element["name"]
+		wig = element["wight"]
+		leng = element["lenght"]
+		document.add_picture(name, wight = wig, lenght = leng)	
+
